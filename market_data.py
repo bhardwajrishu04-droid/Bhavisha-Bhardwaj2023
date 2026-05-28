2c590f087e039111ce61903516dc59c4eb639d08
Deepak Bhardwaj <bhardwaj.rishu04@gmail.com>
2026-05-28 19:20:05 +0530

    Delete market_data.py

--- market_data.py
+++
@@ -1,7 +0,0 @@
-
-import yfinance as yf
-import streamlit as st
-
-@st.cache_data(ttl=120)
-def fetch_stock_data(symbol, period="1mo", interval="1d"):
-    return yf.download(symbol, period=period, interval=interval)
