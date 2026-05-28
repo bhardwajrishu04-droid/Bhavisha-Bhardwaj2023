72e42bcc000a02ec9fbc634bc30a65f82698b7d2
Deepak Bhardwaj <bhardwaj.rishu04@gmail.com>
2026-05-28 19:21:11 +0530

    Delete websocket_service.py

--- websocket_service.py
+++
@@ -1,9 +0,0 @@
-
-from kiteconnect import KiteTicker
-
-class LiveMarketFeed:
-    def __init__(self, api_key, access_token):
-        self.kws = KiteTicker(api_key, access_token)
-
-    def connect(self):
-        self.kws.connect(threaded=True)
