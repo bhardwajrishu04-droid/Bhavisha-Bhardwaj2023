880306f4a85dea1b1cd8b8110da7d874a6d0932c
Deepak Bhardwaj <bhardwaj.rishu04@gmail.com>
2026-05-28 19:21:42 +0530

    Delete risk.py

--- risk.py
+++
@@ -1,8 +0,0 @@
-
-def calculate_position_size(balance, risk_pct, stop_loss_points):
-    risk_amount = balance * (risk_pct / 100)
-
-    if stop_loss_points <= 0:
-        return 0
-
-    return int(risk_amount / stop_loss_points)
